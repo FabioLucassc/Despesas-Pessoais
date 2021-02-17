b396e928cd79d7f467d27c5a00fd20506149d850
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2021-02-17 17:57:51 -0300

    R$

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -11,9 +11,7 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  
   final _transactions = [
-   
     Transaction(
       id: 't1',
       title: 'Novo Tênis de Corrida',
@@ -26,7 +24,6 @@ class MyHomePage extends StatelessWidget {
       value: 211.30,
       date: DateTime.now(),
     ),
-  
   ];
 
   @override
@@ -36,12 +33,9 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
-
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-
         children: <Widget>[
-       
           Container(
             child: Card(
               color: Colors.blue,
@@ -49,71 +43,51 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          
           Column(
-
             children: _transactions.map((tr) {
-              
               return Card(
-               
-                child: Row(
-                 
-                  children: <Widget>[
-
-                    Container(
-
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-
-                      decoration: BoxDecoration(
+                  child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'R\$  ${tr.value.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
                       ),
-
-                      padding: EdgeInsets.all(10),
-                      
-                      child: Text(
-
-                        tr.value.toString(),
-
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tr.title,
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
+                          fontSize: 16,
                         ),
-
                       ),
-
-                    ),
-                    
-                    Column(
-                    
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      
-                      children: <Widget>[
-                        Text(
-                          tr.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
+                      Text(
+                        tr.date.toString(),
+                        style: TextStyle(
+                          color: Colors.grey,
                         ),
-
-                        Text(
-                          tr.date.toString(),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                )
-              );
+                      ),
+                    ],
+                  )
+                ],
+              ));
             }).toList(),
           )
         ],
