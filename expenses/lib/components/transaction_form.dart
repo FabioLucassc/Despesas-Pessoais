79cf8d7ca798fd9a9b168413e3d166a0e6558ae9
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2021-02-19 22:04:08 -0300

    Aviso de Campo Vazio

--- expenses/lib/components/transaction_form.dart
+++ expenses/lib/components/transaction_form.dart
@@ -8,21 +8,62 @@ class TransactionForm extends StatelessWidget {
 
   TransactionForm(this.onSubmit);
 
-  _submitForm() {
-    final title = titleController.text;
-    final value = double.tryParse(valueController.text) ?? 0.0;
+  // _submitForm() {
+  //   final title = titleController.text;
+  //   final value = double.tryParse(valueController.text) ?? 0.0;
 
-    if (title.isEmpty || value <= 0) {
-      return AlertDialog(
-        title: Text('Campos Vazios!'),
-      );
-    } else {
-      onSubmit(title, value);
-    }
-  }
+  //   if (title.isEmpty || value <= 0) {
+  //     return;
+  //   } else {
+  //     onSubmit(title, value);
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
+    _submitForm() {
+      final title = titleController.text;
+      final value = double.tryParse(valueController.text) ?? 0.0;
+
+      if (title.isEmpty || value <= 0) {
+        
+        return showDialog(
+          
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text("Ops... Falta Algo!"),
+                content: Text("Existem Campos Vazios!"),
+                actions: <Widget>[
+                  new FlatButton(
+                    child: Text("OK"),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              );
+            });
+      } else {
+        onSubmit(title, value);
+      }
+    }
+
+    // void showVoidField() {
+    //   showDialog(
+    //       context: context,
+    //       builder: (BuildContext context) {
+    //         return AlertDialog(
+    //           title: Text("My title"),
+    //           content: Text("This is my message."),
+    //           actions: <Widget>[
+    //             new FlatButton(
+    //               child: Text("OK"),
+    //               onPressed: () {return;},
+    //             ),
+    //           ],
+    //         );
+    //       });
+    // }
+
     return Card(
       elevation: 5,
       child: Padding(
