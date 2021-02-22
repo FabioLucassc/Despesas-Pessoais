809bcdaf8d363939265a61c0e561784be4f2f5e0
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2021-02-22 01:17:45 -0300

    Refatoração

--- expenses/lib/components/transaction_form.dart
+++ expenses/lib/components/transaction_form.dart
@@ -1,4 +1,3 @@
-import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
@@ -7,39 +6,47 @@ import 'package:flutter_masked_text/flutter_masked_text.dart';
 class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   // final valueController = TextEditingController();
-  final valueController = MoneyMaskedTextController(decimalSeparator: '.', thousandSeparator: ',');
+  final valueController =
+      MoneyMaskedTextController(decimalSeparator: '.', thousandSeparator: ',');
 
   final Function(String, double) onSubmit;
 
+ 
+
   TransactionForm(this.onSubmit);
-
-  // _submitForm() {
-  //   final title = titleController.text;
-  //   final value = double.tryParse(valueController.text) ?? 0.0;
-
-  //   if (title.isEmpty || value <= 0) {
-  //     return;
-  //   } else {
-  //     onSubmit(title, value);
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
     _submitForm() {
+
       final title = titleController.text;
-      // final String valueText = valueController.text;
-      // final valueini = valueController.text.re;
-      final value = double.tryParse(valueController.text.replaceAll(',', '')) ?? 0.0;
+      final value =
+          double.tryParse(valueController.text.replaceAll(',', '')) ?? 0.0;
       print(valueController.text);
 
-      if (title.isEmpty || value <= 0) {
+      if (title.isEmpty) {
         return showDialog(
             context: context,
             builder: (BuildContext context) {
               return AlertDialog(
                 title: Text("Ops... Falta Algo!"),
-                content: Text("Existem Campos Vazios!"),
+                content: Text("Descrição Vazia!"),
+                actions: <Widget>[
+                  new FlatButton(
+                    child: Text("OK"),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              );
+            });
+   
+      } else if (value <= 0) {
+        return  showDialog(
+            context: ((context)),
+            builder: (_) {
+              return AlertDialog(
+                title: Text("Ops... Falta Algo!"),
+                content: Text("Erro ao inserir o valor!"),
                 actions: <Widget>[
                   new FlatButton(
                     child: Text("OK"),
@@ -53,23 +60,6 @@ class TransactionForm extends StatelessWidget {
       }
     }
 
-    // void showVoidField() {
-    //   showDialog(
-    //       context: context,
-    //       builder: (BuildContext context) {
-    //         return AlertDialog(
-    //           title: Text("My title"),
-    //           content: Text("This is my message."),
-    //           actions: <Widget>[
-    //             new FlatButton(
-    //               child: Text("OK"),
-    //               onPressed: () {return;},
-    //             ),
-    //           ],
-    //         );
-    //       });
-    // }
-
     return Card(
       elevation: 5,
       child: Padding(
@@ -78,22 +68,22 @@ class TransactionForm extends StatelessWidget {
           children: <Widget>[
             TextField(
               controller: titleController,
+              textInputAction: TextInputAction.next,
               decoration: InputDecoration(
                 labelText: 'Titulo',
               ),
             ),
             TextField(
               controller: valueController,
+              onSubmitted: (_) => _submitForm(),
               keyboardType: TextInputType.numberWithOptions(decimal: true),
               decoration: InputDecoration(
                 labelText: 'Valor R\$',
               ),
               inputFormatters: [
                 FilteringTextInputFormatter.digitsOnly,
-                // RealInputFormatter(centavos: true),
               ],
             ),
-            
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:despesas_pessoais/components/transaction_form.dart';
 import './components/transaction_user.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'dart:math';
+import './components/transaction_form.dart';
+import './components/transaction_list.dart';
+import './models/transaction.dart';
 
 import 'package:intl/intl.dart';
 
@@ -18,17 +23,60 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(null);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.black,
       appBar: AppBar(
+        centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.black87,
         title: Text(
           'Despesas Pessoais',
-          textAlign: TextAlign.center,
         ),
+        actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: () {})],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -41,10 +89,15 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            TransactionUser(),
+            TransactionList(_transactions),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
