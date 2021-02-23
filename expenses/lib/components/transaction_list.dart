375984ed845b54513adb4f08fd699fe899ac9ee3
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2021-02-23 13:17:16 -0300

    Transaction Void

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -11,71 +11,90 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: transaction.length,
-        itemBuilder: (ctx, index) {
-          final tr = transaction[index];
-
-          return Card(
-              child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10,
+      child: transaction.isEmpty
+          ? Column(
+              children: <Widget>[
+                SizedBox(height: 20),
+                Text(
+                  'Nenhuma Transação Cadastrada!',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(40),
-                      bottomLeft: Radius.circular(40),
-                      bottomRight: Radius.circular(40),
-                      topLeft: Radius.circular(40),
-                    ),
-                    border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
-                    )),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                    NumberFormat.currency(
-                      symbol: 'R\$',
-                      decimalDigits: 2,
-                    ).format(tr.value),
-                    overflow: TextOverflow.ellipsis,
-
-                    // NumberFormat.compactSimpleCurrency(
-                    //         locale: 'pt-br', decimalDigits: 2, name: 'R\$')
-                    //     .format(tr.value),
-
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    )),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tr.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
+                SizedBox(height: 30),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
-                  Text(
-                    // tr.date.toString(),
-                    DateFormat("d 'de' MMM 'de' y (E)").format(tr.date),
-                    style: TextStyle(
-                      color: Colors.grey,
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemCount: transaction.length,
+              itemBuilder: (ctx, index) {
+                final tr = transaction[index];
+
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(40),
+                            bottomLeft: Radius.circular(40),
+                            bottomRight: Radius.circular(40),
+                            topLeft: Radius.circular(40),
+                          ),
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          )),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                          NumberFormat.currency(
+                            symbol: 'R\$',
+                            decimalDigits: 2,
+                          ).format(tr.value),
+                          overflow: TextOverflow.ellipsis,
+
+                          // NumberFormat.compactSimpleCurrency(
+                          //         locale: 'pt-br', decimalDigits: 2, name: 'R\$')
+                          //     .format(tr.value),
+
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          )),
                     ),
-                  ),
-                ],
-              )
-            ],
-          ));
-        },
-      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tr.title,
+                          // style: Theme.of(context).textTheme.headline6,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          // tr.date.toString(),
+                          DateFormat("d 'de' MMM 'de' y (E)").format(tr.date),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ));
+              },
+            ),
     );
   }
 }
